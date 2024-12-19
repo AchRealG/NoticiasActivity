c45026cb87a8b2113da22473c5396ed9159e9f45
AchRealG <achmbomag@alu.edu.gva.es>
2024-12-19 19:28:19 +0100

    commit

--- app/src/main/java/com/example/noticiasactivity/MainActivity.kt
+++ app/src/main/java/com/example/noticiasactivity/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.noticiasactivity.databinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var NoticiasAdapter: NoticiasAdapter
+    private lateinit var linearLayout: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        NoticiasAdapter = NoticiasAdapter(mutableListOf())
+        var linearLayoutManager = LinearLayoutManager(this)
+        binding.recyclerView.apply {
+            var layoutManager = linearLayoutManager
+            var adapter = NoticiasAdapter
         }
-    }
 }
--- app/src/main/java/com/example/noticiasactivity/NoticiasAdapter.kt
+++ app/src/main/java/com/example/noticiasactivity/NoticiasAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.noticiasactivity
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -14,5 +15,16 @@ class NoticiasAdapter (private val noticias: List<Noticias>){
         val binding = ItemNoticiaBinding.bind(view)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val view =
+            LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_noticias, parent, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int = users.size
 
 }
